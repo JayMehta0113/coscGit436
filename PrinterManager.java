fbbc79c7da1b697d3ba6b5da0acd5b554d003cb3
Bryan Lehosky <90706529+BLehosky@users.noreply.github.com>
2024-02-28 13:15:38 -0500

    Add files via upload

---
+++ PrinterManager.java
@@ -0,0 +1,14 @@
+import Singleton.Printer;
+
+public class PrinterManager extends Printer{
+    private static Printer singleInstance;
+    private static PrinterManager[] printers;
+
+    private PrinterManager() {
+        
+    }
+    public static String getInstance() {
+
+    }
+    public static 
+}
