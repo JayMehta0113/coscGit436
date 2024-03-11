0f28b087a4eec3572d40a59867f1a5bc4326b81d
JayMehta0113 <159817371+JayMehta0113@users.noreply.github.com>
2024-03-11 17:46:11 -0400

    Add files via upload

---
+++ StringArray.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+/*
+ * 6.
+ * 7.
+ */
+
+public class StringArray implements Iterable <String>{
+    
+    String[] values;
+
+    public StringArray(String[] values){
+        super();
+        this.values = values;
+    }
+
+    public Iterator iterator(){
+        return new ArrayIterator();
+    }
+
+    class ArrayIterator implements Iterator <String>{
+        int current = 0;
+
+        public boolean hasNext(){
+            if (current < StringArray.this.values.length){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public String next(){
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return values[current++];
+        }
+
+        public void set(int index, String value){
+            values[index]=value;
+        }
+
+        public int length(){
+            return values.length;
+        }
+
+    }
+
+    public static void main(String[] args){
+        String[] strings = new String[]{"one", "two", "three", "four", "five"};
+
+    StringArray array = new StringArray(strings);
+    Iterator<String> iterator = array.iterator();
+
+    while (iterator.hasNext()) {
+        System.out.println(iterator.next());
+    }
+    }
+
+}
+
